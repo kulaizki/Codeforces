4df2a68c255063d1bfb6e43d8e5a80740ee24c59
kulaizki <kulaizke@gmail.com>
2023-03-08 23:01:46 +0800

    left boundary & right boundary

---
+++ ideal-point.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve() {
+
+    int n, k;
+    cin >> n >> k;
+
+    int L = 0, R = 50;
+    while (n--) {
+        int l, r;
+        cin >> l >> r;
+        if (l <= k && k <= r) {
+            L = max(L, l), R = min(R, r);
+        }
+    }
+
+    cout << (L == R ? "YES\n" : "NO\n" );
+}
+
+int main() {
+
+    ios_base::sync_with_stdio(0); cin.tie(0);
+
+    int t;
+    cin >> t;
+
+    while (t--) solve();
+}
