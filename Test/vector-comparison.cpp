62474ad94dcee26158d6d3ab446d31fcd70c77cc
kulaizki <kulaizke@gmail.com>
2023-03-11 14:16:58 +0800

    vector explicit ==

---
+++ Test/vector-comparison.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    vector<int> v1 = {1, 2, 3};
+    vector<int> v2 = {1, 2, 3};
+    vector<int> v3 = {1, 3, 2};
+
+    cout << (v1 == v2) << '\n';
+    cout << (v1 == v3) << '\n';
+}
