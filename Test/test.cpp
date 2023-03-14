59c22ffc550087b77c57cdb37e1318a73fd453d3
kulaizki <kulaizke@gmail.com>
2023-03-14 11:46:35 +0800

    template

--- Test/test.cpp
+++ Test/test.cpp
@@ -1,19 +1,19 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 #define all(x) (x).begin(), (x).end()
 #define ll long long
-#define pb push_back
+#define pub push_back
+#define sz(x) (int)(x).size()
 
 void solve() {
 
     // kulaizki
-    
+
 }
 
 int main() {
+
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
     int t;
     cin >> t;
