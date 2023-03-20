fd5146ff82325ee61801bdd82a5d5d9eb325bb84
kulaizki <kulaizke@gmail.com>
2023-03-20 10:16:52 +0800

    format

--- Contests/Contest Round 859 Div. 4/E.cpp
+++ Contests/Contest Round 859 Div. 4/E.cpp
@@ -9,27 +9,11 @@ typedef unsigned long long ull;
 
 ll n, m, k, q, l, r, x, y, z;
 const ll template_array_size = 1e6 + 585;
-ll a[template_array_size];
-ll b[template_array_size];
-ll c[template_array_size];
-string s, t;
-ll ans = 0;
 
 void solve(int tc = 0) {
 
-    ll m = 0, h = 0;
-    cin >> n;
-    for (ll i = 0; i < n; ++i) {
-        ll x;
-        cin >> x;
-        if (x % 2 == 0) {
-            m += x;
-        } else {
-            h += x;
-        }
-    }
+    
 
-    cout << (m > h ? "YES\n" : "NO\n"); 
 }
 
 int main() {
