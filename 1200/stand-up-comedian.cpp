4b75941b9558049b6ae71a8023528bf3162e1354
kulaizki <kulaizke@gmail.com>
2023-03-21 23:10:06 +0800

    a + min(b, c) * 2 + min(a + 1, abs(b - c) + d)

---
+++ 1200/stand-up-comedian.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define activate {ios_base::sync_with_stdio(false);}
+#define poof(a) {for (const auto& x : a) cout << x << ' '; cout << ' ';}
+#define mugen {cin.tie(NULL); cout.tie(NULL);}
+typedef long long ll;
+typedef long double lld;
+typedef unsigned long long ull;
+
+const ll template_array_size = 1e6 + 585;
+const lld pi = 3.14159265358979323846;
+// const ll mod = 1000000007;
+// const ll mod = 998244353;
+// ll mod;
+
+void solve(int tc = 0) {
+
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+
+    int ans = 1;
+    if (a > 0) {
+        ans = a + min(b, c) * 2 + min(a + 1, abs(b - c) + d);
+    }
+
+    cout << ans << '\n';
+}
+
+int main() {
+
+    activate mugen
+    int tc = 1;
+    cin >> tc;
+    for (int t = 0; t < tc; t++) solve(t);
+}
