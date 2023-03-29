6243bcfba3368ac27d61a6de76c9ac8aae5b9f6c
kulaizki <kulaizke@gmail.com>
2023-03-29 19:57:26 +0800

    sum, negs, a[i] = -a[i]

---
+++ 1100/negatives-and-positives.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define activate {ios_base::sync_with_stdio(false);}
+#define poof(a) {for (const auto& x : a) cout << x << ' ';}
+#define mugen {cin.tie(NULL); cout.tie(NULL);}
+typedef long long ll;
+typedef long double lld;
+typedef unsigned long long ull;
+
+const ll template_array_size = 1e6 + 585;
+const lld pi = 3.14159265358979323846;
+// const ll mod = 1000000007;
+// const ll mod = 998244353;
+// ll mod;
+
+void solve(int tc = 0) {
+
+    int n; cin >> n;
+    vector<int> a(n);
+    ll sum = 0;
+    ll negs = 0;
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+        if (a[i] < 0) {
+            ++negs;
+            a[i] = -a[i];
+        }
+        sum += a[i];
+    }
+
+    sort(a.begin(), a.end());
+    if (negs & 1) sum -= 2 * a[0];
+    cout << sum << '\n';
+}
+
+int main() {
+
+    activate mugen
+    int tc = 1;
+    cin >> tc;
+    for (int t = 0; t < tc; t++) solve(t);
+}
