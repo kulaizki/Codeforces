ef002edfc9e1d321f5cdaafedc987a7444a15786
kulaizki <kulaizke@gmail.com>
2023-07-02 15:08:40 +0800

    dx & dy = -1, nx = i + dx, ny = j + dy

--- 1100/minesweeper.cpp
+++ 1100/minesweeper.cpp
@@ -6,33 +6,52 @@ int main() {
 
     int n, m;
     cin >> n >> m;
-    vector<vector<char>> v(n, vector<char>(m));
+    vector<vector<char>> field(n, vector<char>(m));
 
-    for (auto &i : v) {
-        for (auto &j : i) {
-            cin >> j;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            cin >> field[i][j];
         }
     }
 
-    int ans = 0;
-    for (int i = 0; i < n && ans == 0; ++i) {
-        for (int j = 0; j < m && ans == 0; ++j) {
-            if (v[i][j] == '.') {
-                if (i > 0 && j > 0 && i <  n - 1 && j < m - 1) {
-                    if (v[i - 1][j - 1] == '*' || v[i - 1][j] == '*' || 
-                        v[i - 1][j + 1] == '*' || v[i][j - 1] == '*' ||
-                        v[i][j + 1] == '*' || v[i + 1][j - 1] == '*' ||
-                        v[i + 1][j] == '*' || v[i + 1][j + 1] == '*') {
-                            ans = 1;
+    bool valid = true;
+    for (int i = 0; i < n && valid; ++i) {
+        for (int j = 0; j < m && valid; ++j) {
+            if (field[i][j] == '.') {
+                for (int dx = -1; dx <= 1; ++dx) {
+                    for (int dy = -1; dy <= 1; ++dy) {
+                        int nx = i + dx;
+                        int ny = j + dy;
+                        if (nx >= 0 && nx < n && ny >= 0 && ny < m && field[nx][ny] == '*') {
+                            valid = false;
+                            break;
+                        }
+                    }
+
+                    if (!valid) {
+                        break;
                     }
                 }
-            } else if (v[i][j] != '*') {
-                // for (int k = 0; k < 8; k++) {
+            } else if (field[i][j] >= '1' && field[i][j] <= '8') {
+                int k = field[i][j] - '0';
+                int bombs = 0;
+                for (int dx = -1; dx <= 1; ++dx) {
+                    for (int dy = -1; dy <= 1; ++dy) {
+                        int nx = i + dx;
+                        int ny = j + dy;
 
-                // }
+                        if (nx >= 0 && nx < n && ny >= 0 && ny < m && field[nx][ny] == '*') {
+                            bombs++;
+                        }
+                    }
+                }
+
+                if (bombs != k) {
+                    valid = false;
+                }
             }
         }
     }
 
-    cout << (ans == 0 ? "YES" : "NO");
+    cout << (valid ? "YES" : "NO");
 }
