03ec436ca6a8fc3fe864c80e0bfbfac66c9b35f9
kulaizki <kulaizke@gmail.com>
2023-07-03 19:24:11 +0800

    v[x]++

---
+++ 800/destroyer.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define activate {ios_base::sync_with_stdio(false);}
+#define poof(a) {for (const auto& x : a) cout << x << ' ';}
+#define mugen {cin.tie(NULL); cout.tie(NULL);}
+typedef long long ll;
+typedef long double lld;
+typedef unsigned long long ull;
+
+const ll template_array_size = 1e6 + 585;
+const lld pi = 3.14159265358979323846;
+// const ll mod = 1000000007;
+// const ll mod = 998244353;
+// ll mod;
+
+void solve(int tc = 0) {
+
+    int n;
+    cin >> n;
+
+    vector<int> v(n + 1);
+
+    for (int i = 0; i < n; ++i) {
+        int x;
+        cin >> x;
+        if (x < n) {
+            v[x]++;
+        } else {
+            v[n] = template_array_size;
+        }
+    }
+
+    bool flag = true;
+    for (int i = 1; i <= n; ++i) {
+        if (v[i] > v[i - 1]) {
+            flag = false;
+            break;
+        }
+    }
+    
+    cout << (flag ? "YES\n" : "NO\n");
+}
+
+int main() {
+
+    activate mugen
+    int tc = 1;
+    cin >> tc;
+    for (int t = 0; t < tc; t++) solve(t);
+}
