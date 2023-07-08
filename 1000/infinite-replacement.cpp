1b9e47f31283d556b5d6058e6a16dc5196fa1b67
kulaizki <kulaizke@gmail.com>
2023-07-08 20:26:29 +0800

    ll x = pow(2, s.length()) | pow return double

---
+++ 1000/infinite-replacement.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define activate {ios_base::sync_with_stdio(false);}
+#define poof(a) {for (const auto& x : a) cout << x << ' ';}
+#define mugen {cin.tie(NULL); cout.tie(NULL);}
+typedef long long ll;
+typedef long double lld;
+typedef unsigned long long ull;
+
+const ll template_array_size = 1e6 + 585;
+const lld pi = 3.14159265358979323846;
+// const ll mod = 1000000007;
+// const ll mod = 998244353;
+// ll mod;
+
+void solve(int tc = 0) {
+
+    string s, t;
+    cin >> s >> t;
+
+    if (t == "a") {
+        cout << "1\n";
+    }
+    else if (t.find('a') != string::npos) {
+        cout << "-1\n";
+    }
+    else {
+        ll x = pow(2, s.length());
+        cout << x << '\n';
+    }
+}
+
+int main() {
+
+    activate mugen
+    int tc = 1;
+    cin >> tc;
+    for (int t = 0; t < tc; t++) solve(t);
+}
