0eb360ad6c24ca3493a836c59982aceec5caa2e2
kulaizki <kulaizke@gmail.com>
2023-07-15 18:41:47 +0800

    v.back() <= v[0] + v[1]

---
+++ 1100/maximum-sum.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define kulaizki {ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);}
+#define poof(a) {for (const auto& x : a) cout << x << ' ';}
+typedef long long ll;
+typedef long double lld;
+typedef unsigned long long ull;
+
+const ll template_array_size = 1e6 + 585;
+const lld pi = 3.14159265358979323846;
+// const ll mod = 1000000007;
+// const ll mod = 998244353;
+// ll mod;
+
+void solve(int tc = 0) {
+
+    int n, k;
+    cin >> n >> k;
+
+    vector<ll> v(n);
+    for (auto &x : v) cin >> x;
+    sort(v.begin(), v.end());
+
+    while (k--) {
+        if (v.back() <= v[0] + v[1]) {
+            v.pop_back();
+        } else {
+            // iterator pointing to the position after index
+            v.erase(v.begin(), v.begin() + 2);
+        }
+    }
+
+    ll sum = 0;
+    for (const auto &x : v) sum += x;
+
+    cout << sum << '\n';
+}
+
+int main() {
+
+    kulaizki
+    int tc = 1;
+    cin >> tc;
+    for (int t = 0; t < tc; t++) solve(t);
+}
