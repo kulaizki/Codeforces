1c68714c4af390ccd58c11915e7b4d9807f36bf2
kulaizki <kulaizke@gmail.com>
2023-07-22 00:53:05 +0800

    abcd

---
+++ Contests/Contest Round 886 (Div. 4)/B.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define kulaizki {ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);}
+#define poof(a) {for (const auto& x : a) cout << x << ' ';}
+#define pb push_back
+
+typedef long long ll;
+typedef long double lld;
+typedef unsigned long long ull;
+
+const ll template_array_size = 1e6 + 585;
+const lld pi = 3.14159265358979323846;
+// const ll mod = 1000000007;
+// const ll mod = 998244353;
+// ll mod;
+
+void solve(int tc = 0) {
+
+    int n;
+    cin >> n;
+
+    int ans = 1, mx = 0;
+    for (int i = 1; i <= n; ++i) {
+        int a, b;
+        cin >> a >> b;
+        if (a <= 10 && mx < b) {
+            ans = i;
+            mx = b;
+        }
+    }
+
+    cout << ans << '\n';
+}
+
+int main() {
+
+    kulaizki
+    int tc = 1;
+    cin >> tc;
+    for (int t = 0; t < tc; t++) solve(t);
+}
