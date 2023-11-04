30156299d980e154b29a9a546ce061f583ce0cc6
kulaizki <kulaizke@gmail.com>
2023-11-04 17:09:29 +0800

    ans = 2 * 1e9, ans = min(ans, d + (s - 1) / 2)

---
+++ 900/the-corridor-or-there-and-back-again.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define kulaizki {ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);}
+#define poof(a) {for (const auto& x : a) cout << x << ' ';}
+#define pb push_back
+
+typedef long long int ll;
+typedef long double lld;
+typedef unsigned long long ull;
+
+const ll template_array_size = 1e6 + 585;
+const lld pi = 3.14159265358979323846;
+// const ll mod = 1000000007;
+// const ll mod = 998244353;
+// ll mod;
+
+void solve(int tc = 0) {
+
+    int n;
+    cin >> n;
+    
+    int ans = 2 * 1e9;
+
+    for (int i = 0; i < n; ++i) {
+        int d, s;
+        cin >> d >> s;
+        ans = min(ans, d + (s - 1) / 2);
+    }
+
+    cout << ans << '\n';
+}
+
+int main() {
+
+    kulaizki
+    int tc = 1;
+    cin >> tc;
+    for (int t = 0; t < tc; t++) solve(t);
+}
