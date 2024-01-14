57ff1d65f425b961b7047fd8e28eb375988cb896
kulaizki <kulaizke@gmail.com>
2024-01-14 17:00:06 +0800

    s[0] << s.size() - 2 << s[s.size() - 1]

---
+++ 800/way-too-long-words.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+#define kulaizki {ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);}
+#define poof(a) {for (const auto& x : a) cout << x << ' ';}
+#define pb push_back
+
+typedef long long ll;
+typedef long double lld;
+typedef unsigned long long ull;
+
+const ll template_array_size = 1e6 + 585;
+const lld pi = 3.14159265358979323846;
+// const ll mod = 1000000007;
+// const ll mod = 998244353;
+// ll mod;
+
+void solve(int tc = 0) {
+
+    string s;
+    cin >> s;
+
+    if (s.length() > 10) {
+        cout << s[0] << s.size() - 2 << s[s.size() - 1];
+    } else {
+        cout << s;
+    }
+
+    cout << '\n';
+}
+
+int main() {
+
+    kulaizki
+    int tc = 1;
+    cin >> tc;
+    for (int t = 0; t < tc; t++) solve(t);
+}
