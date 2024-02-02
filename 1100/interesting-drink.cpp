aa48000c8b222b88e7fc86524484956237a2830f
kulaizki <kulaizke@gmail.com>
2024-02-02 10:27:54 +0800

    TLE, O(n^logq)

---
+++ 1100/interesting-drink.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define kulaizki {ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);}
+#define poof(a) {for (const auto& x : a) cout << x << ' ';}
+#define pb push_back
+
+typedef long long int ll;
+typedef long double lld;
+typedef unsigned long long ull;
+
+const ll template_array_size = 1e6 + 585;
+const lld pi = 3.14159265358979323846;
+// const ll mod = 1000000007;
+// const ll mod = 998244353;
+// ll mod;
+
+void solve(int tc = 0) {
+
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (auto &x : a) cin >> x;
+    sort(a.begin(), a.end());
+
+    int q;
+    cin >> q;
+    vector<int> b(q);
+    for (auto &x : b) cin >> x;
+
+    for (int i = 0; i < q; ++i) {
+        int j, c;
+        for (j = 0, c = 0; j < n && b[i] >= a[j]; ++j, c++) {}
+        cout << c << '\n';
+    }
+}
+
+int main() {
+
+    kulaizki
+    int tc = 1;
+    // cin >> tc;
+    for (int t = 0; t < tc; t++) solve(t);
+}
