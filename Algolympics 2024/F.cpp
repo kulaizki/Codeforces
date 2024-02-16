36b7037cc3bf3cda9e4064e67abf4d29b8dbb10a
kulaizki <kulaizke@gmail.com>
2024-02-17 03:30:07 +0800

    feat: add solution | TLE lambda sort

---
+++ Algolympics 2024/F.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+#define kulaizki {ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);}
+#define poof(a) {for (const auto& x : a) cout << x << ' ';}
+#define pb push_back
+
+typedef long long ll;
+typedef long double lld;
+typedef unsigned long long ull;
+
+const ll template_array_size = 1e6 + 585;
+const lld pi = 3.14159265358979323846;
+// const ll mod = 1000000007;
+// const ll mod = 998244353;
+// ll mod;
+
+void solve(int tc = 0) {
+    int n;
+    cin >> n;
+
+    vector<int> v(n);
+    for (auto &x : v) cin >> x;
+
+    int minGreaterIndex = -1; // Initialize to an invalid index
+
+    for (int i = 0; i < n; ++i) {
+        if (minGreaterIndex == -1 || v[i] < v[minGreaterIndex]) {
+            cout << i + 1 << ' ';
+        } else {
+            cout << minGreaterIndex + 1 << ' ';
+        }
+
+        // Update minGreaterIndex if the current element is greater than the element at minGreaterIndex
+        if (minGreaterIndex == -1 || v[i] > v[minGreaterIndex]) {
+            minGreaterIndex = i;
+        }
+    }
+
+    cout << '\n';
+}
+
+int main() {
+
+    kulaizki
+    int tc = 1;
+    cin >> tc;
+    for (int t = 0; t < tc; t++) solve(t);
+}
