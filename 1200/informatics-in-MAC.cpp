07addc40e5545fc53d002e6dba0b25ef432af406
kulaizki <kulaizke@gmail.com>
2024-03-24 10:48:56 +0800

    feat: add solution | k == 2, mex1 mex2, (i, i+1) & (i+2, n)

--- 1200/informatics-in-MAC.cpp
+++ 1200/informatics-in-MAC.cpp
@@ -24,25 +24,33 @@ void solve(int tc = 0) {
     int n;
     cin >> n;
     vector<int> a(n);
-    int mn = INT_MAX;
+    vector<int> cnt1(n + 1), cnt2(n + 1);
     for (auto &x : a) {
         cin >> x;
-        mn = min(mn, x);
+        cnt2[x]++;
     }
 
-    set<int> diff;
-    vector<int> b = a;
-    sort(b.begin(), b.end());
-    int i;
-    for (i = 1; i < n && b[i] == b[i - 1]; ++i) {}
-    for (; i < n; ++i) {
-        diff.insert(abs(b[i] - b[i - 1]));
+    int mex1 = 0, mex2 = 0;
+    while (cnt2[mex2])
+        ++mex2;
+    for (int i = 0; i < n; ++i) {
+        cnt1[a[i]]++;
+        if (--cnt2[a[i]] == 0 && mex2 > a[i]) {
+            mex2 = a[i];
+        }
+        while (mex2 && !cnt2[mex2 - 1])
+            --mex2;
+        while (cnt1[mex1])
+            ++mex1;
+        if (mex1 == mex2) {
+            cout << "2\n";
+            cout << 1 << ' ' << i + 1 << '\n';
+            cout << i + 2 << ' ' << n << '\n';
+            return;
+        }
     }
 
-    if (diff.size() < 2) {
-        cout << -1;
-    } else {
-
+    cout << "-1\n";
 }
 
 int main() {
