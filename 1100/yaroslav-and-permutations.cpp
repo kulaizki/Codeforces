993001be5623b448745a331683546be687b28d60
kulaizki <kulaizke@gmail.com>
2024-03-26 15:35:24 +0800

    feat: add solution | a[1000] <= (n + 1) / 2

---
+++ 1100/yaroslav-and-permutations.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define kulaizki {ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);}
+#define poof(a) {for (const auto& x : a) cout << x << ' ';}
+#define pb push_back
+
+typedef long long int ll;
+typedef long double lld;
+typedef unsigned long long ull;
+
+const ll template_array_size = 1e6 + 585;
+const lld pi = 3.14159265358979323846;
+// const ll mod = 1000000007;
+// const ll mod = 998244353;
+// ll mod;
+
+void solve(int tc = 0) {
+
+    int n;
+    cin >> n;
+    vector<int> a(1001);
+    for (int i = 0; i < n; ++i) {
+        int x;
+        cin >> x;
+        a[x]++;
+    }
+
+    sort(a.begin(), a.end());
+    cout << (a[1000] <= (n + 1) / 2 ? "YES" : "NO") << '\n';
+}
+
+int main() {
+
+    kulaizki
+    int tc = 1;
+    // cin >> tc;
+    for (int t = 0; t < tc; t++) solve(t);
+}
