818635f3991de7bc517fa60c1fbd408f9e407dbe
kulaizki <kulaizke@gmail.com>
2024-03-28 15:49:08 +0800

    feat: add solution | reverse problem, m++ & m /= 2 or n - m

---
+++ 1400/two-buttons.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define kulaizki {ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);}
+#define poof(a) {for (const auto& x : a) cout << x << ' ';}
+#define pb push_back
+
+typedef long long int ll;
+typedef long double lld;
+typedef unsigned long long ull;
+
+const ll template_array_size = 1e6 + 585;
+const lld pi = 3.14159265358979323846;
+// const ll mod = 1000000007;
+// const ll mod = 998244353;
+// ll mod;
+
+void solve(int tc = 0) {
+
+    int n, m;
+    cin >> n >> m;
+
+    int ans = 0;
+    if (m < n) {
+        ans = n - m;
+        m = n;
+    } else {
+        while (m > n) {
+            if (m & 1) {
+                m++;
+            } else {
+                m /= 2;
+            }
+            ans++;
+        }
+    }
+
+    cout << (m == n ? ans : ans + n - m) << '\n';
+}
+
+int main() {
+
+    kulaizki
+    int tc = 1;
+    // cin >> tc;
+    for (int t = 0; t < tc; t++) solve(t);
+}
