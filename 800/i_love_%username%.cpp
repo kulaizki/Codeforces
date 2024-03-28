ab2938b0ef40b7aed4d470be377d357ed7e7038f
kulaizki <kulaizke@gmail.com>
2024-03-28 13:29:18 +0800

    feat: add solution | count mid break and max break

---
+++ 800/i_love_%username%.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define kulaizki {ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);}
+#define poof(a) {for (const auto& x : a) cout << x << ' ';}
+#define pb push_back
+
+typedef long long int ll;
+typedef long double lld;
+typedef unsigned long long ull;
+
+const ll template_array_size = 1e6 + 585;
+const lld pi = 3.14159265358979323846;
+// const ll mod = 1000000007;
+// const ll mod = 998244353;
+// ll mod;
+
+void solve(int tc = 0) {
+
+    int n;
+    cin >> n;
+
+    int mn, mx, amazing = 0;
+    cin >> mn;
+    mx = mn;
+    for (int i = 1; i < n; ++i) {
+        int x;
+        cin >> x;
+        if (x < mn) {
+            mn = x;
+            amazing++;
+        } else if (x > mx) {
+            mx = x;
+            amazing++;
+        }
+    }
+
+    cout << amazing;
+}
+
+int main() {
+
+    kulaizki
+    int tc = 1;
+    // cin >> tc;
+    for (int t = 0; t < tc; t++) solve(t);
+}
