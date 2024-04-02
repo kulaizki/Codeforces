71d27becc5e06cdf9362ad1f66f5ca4b0ba59f42
kulaizki <kulaizke@gmail.com>
2024-04-02 18:24:41 +0800

    feat: add solution | hashmap, track winner and max

---
+++ 1000/football.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+#define kulaizki {ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);}
+#define poof(a) {for (const auto& x : a) cout << x << ' ';}
+#define pb push_back
+
+typedef long long ll;
+typedef long double lld;
+typedef unsigned long long ull;
+
+const ll template_array_size = 1e6 + 585;
+const lld pi = 3.14159265358979323846;
+// const ll mod = 1000000007;
+// const ll mod = 998244353;
+// ll mod;
+
+void solve(int tc = 0) {
+
+    int n;
+    cin >> n;
+
+    unordered_map<string, int> mp;    
+    string winner;
+    int mx = 0;
+    for (int i = 0; i < n; ++i) {
+        string s;
+        cin >> s;
+        mp[s]++;
+        if (mp[s] > mx) {
+            mx = mp[s];
+            winner = s;
+        }
+    }
+
+    cout << winner << '\n';
+}
+
+int main() {
+
+    kulaizki
+    int tc = 1;
+    // cin >> tc;
+    for (int t = 0; t < tc; t++) solve(t);
+}
