07eda2d87959fad5c248adc70bc1ecd298a881a8
kulaizki <kulaizke@gmail.com>
2024-04-04 22:51:32 +0800

    feat: add idea

---
+++ 1000/k-String.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+#define kulaizki {ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);}
+#define poof(a) {for (const auto& x : a) cout << x << ' ';}
+#define pb push_back
+
+typedef long long ll;
+typedef long double lld;
+typedef unsigned long long ull;
+
+const ll template_array_size = 1e6 + 585;
+const lld pi = 3.14159265358979323846;
+// const ll mod = 1000000007;
+// const ll mod = 998244353;
+// ll mod;
+
+void solve(int tc = 0) {
+
+    int k;
+    cin >> k;
+    string s;
+    cin >> s;
+
+    
+
+}
+
+int main() {
+
+    kulaizki
+    int tc = 1;
+    cin >> tc;
+    for (int t = 0; t < tc; t++) solve(t);
+}
