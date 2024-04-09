ab57f56c0114aca7a4e31ef626879800002be385
kulaizki <kulaizke@gmail.com>
2024-04-09 20:10:16 +0800

    feat: add solution | count occurence, b string based on q and k

--- 1000/k-String.cpp
+++ 1000/k-String.cpp
@@ -20,19 +20,39 @@ const lld pi = 3.14159265358979323846;
 
 void solve(int tc = 0) {
 
-    int k;
-    cin >> k;
-    string s;
-    cin >> s;
+    int k, count= 0;
+	char ch;
+	cin >> k;
+	string s;
+	cin >> s;
+	int l = s.size();
+	sort (s.begin(), s.end());
+	for (int i = 0; i < s.size(); i++) {
+	    if (i % k == 0){
+	       ch = s[i]; 
+	    }
+	    if (s[i] == ch){
+	        count++;
+	    }
+	}
 
-    //
+	if (l == count && count % k == 0) {
+	    for (int i = 0; i < k; i++) {
+	        for (int l = 0; l < s.size(); l += k) {
+	            cout << s[l];
+	        }
+	    }
+	} else {
+	    cout << "-1";
+	}
 
+    cout << '\n';
 }
 
 int main() {
 
     kulaizki
     int tc = 1;
-    cin >> tc;
+    // cin >> tc;
     for (int t = 0; t < tc; t++) solve(t);
 }
