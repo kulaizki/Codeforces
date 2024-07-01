a820f00d1fcf747278633dc6bce4eec00c9bf3ce
kulaizki <kulaizke@gmail.com>
2024-07-01 15:57:27 +0800

    feat: add idea | linear search

---
+++ 1400/tempCodeRunnerFile.cpp
@@ -0,0 +1,10 @@
+    // int ans = INT_MAX;
+    // for (int i = 0; i < n; ++i) {
+    //     int sum = 0;
+    //     for (int j = i; j < n; ++i) {
+    //         sum += a[j];
+    //     }
+    //     if (sum < t && sum < ans) {
+    //         ans = sum;
+    //     }
+    // }
