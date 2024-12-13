ee494ed4b2a5f49fe472686a85bb2767f53c4497
kulaizki <kulaizke@gmail.com>
2024-12-13 23:57:48 +0800

    feat: update to g++ & use bits/stdc++.h

---
+++ 1200/substring-and-subsequence.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define kulaizki {ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);}
+#define poof(a) {for (const auto& x : a) cout << x << ' ';}
+#define pb push_back
+
+typedef long long ll;
+typedef long double lld;
+typedef unsigned long long ull;
+
+const ll template_array_size = 1e6 + 585;
+const lld pi = 3.14159265358979323846;
+// const ll mod = 1000000007;
+// const ll mod = 998244353;
+// ll mod;
+
+void solve() {
+
+    string a, b;
+    cin >> a;
+    cin >> b;
+
+    int n = a.size();
+    int m = b.size();
+    int ans = n + m;
+
+    for (int i = 0; i < m; ++i) {
+        int j = i;
+        for (auto &c : a) {
+            if (j < m && c == b[j]) ++j;
+        }
+        ans = min(ans, n + m - (j - i));
+    }
+
+    cout << ans << endl;
+}
+
+int main() {
+
+    kulaizki
+    int tc = 1;
+    cin >> tc;
+    for (int t = 0; t < tc; t++) solve();
+}
