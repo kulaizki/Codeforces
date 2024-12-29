4c8f1ee75497b8c34cf209af8e9eb962866d8cc5
kulaizki <kulaizke@gmail.com>
2024-12-29 23:51:01 +0800

    feat: add solution | b - a

---
+++ 800/minimize.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define kulaizki {ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);}
+#define poof(a) {for (const auto& x : a) cout << x << ' ';}
+#define pb push_back
+
+typedef long long ll;
+typedef long double lld;
+typedef unsigned long long ull;
+
+// const ll template_array_size = 1e6 + 777;
+// const lld pi = 3.14159265358979323846;
+// const ll MOD = 1'000'000'007;
+// const ll MOD = 998'244'353;
+// const MAX = 200'007;
+
+void solve() {
+
+    int a, b;
+    cin >> a >> b;
+
+    cout << b - a << '\n';
+}
+
+int main() {
+
+    kulaizki
+    int tc = 1;
+    cin >> tc;
+    for (int t = 0; t < tc; t++) solve();
+}
